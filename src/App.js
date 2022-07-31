6a7d1fe89a113c4c93f9b0169906c75fc2c8726b
Luna Lam <lunalam@Lunas-MacBook-Pro.local>
2022-07-31 11:26:13 -0700

    Added background logo and footer component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import Main from './components/Main';
 import Navbar from './components/Navbar'
+import Main from './components/Main';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className='container'>
       <Navbar />
       <Main />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+
+function Footer() {
+    return(
+        <footer>
+            <small>&copy; A Luna Lam Project. All rights reserved.</small>
+        </footer>
+    )
+}
+
+export default Footer;
